8e92de84e2231d91c6b00db5047685855e4f99e5
carmengalvan <148187458+carmengalvan@users.noreply.github.com>
2024-02-01 14:32:42 +0100

    feat: header component (#16)
    * feat: header component
    
    * fix: radix avatar installation
    
    * feat: integration of the menu with the header

--- src/containers/Views/MyResources/index.tsx
+++ src/containers/Views/MyResources/index.tsx
@@ -1,10 +1,16 @@
+import Header from '@/components/Header';
 import Menu from '@/components/Sidebar';
 
 export function MyResourcesView() {
 	return (
 		<>
-			<div className="hidden flex-col md:flex">
-				<Menu />
+			<div className="flex flex-row w-full h-full">
+				<div className="w-[350px]">
+					<Menu />
+				</div>
+				<div className="w-full">
+					<Header title="Mis recursos" />
+				</div>
 			</div>
 		</>
 	);
